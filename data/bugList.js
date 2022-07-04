c37f04a4a3af222f3b33fca5b27469406262fbcf
Gavin Ovsak <ovsak.gavin@gmail.com>
2022-07-03 20:53:13 -0400

    u

---
+++ data/bugList.js
@@ -0,0 +1,22 @@
+var bugList = {
+  'Gram Positive Cocci': {
+    'Staph Aureus': {
+      'MRSA': true,
+      'MSSA': true
+    },
+    'Streptococcus': true
+  },
+  'Gram Negative Bacilli': {
+    'E. Coli': true,
+    'P. Mirabilis': true,
+    'Klebsiella': true,
+    'Pseudomonas': true,
+    'ESCAPPM': true
+  },
+  'Gram Negative Cocci': {
+    'N. Gonorrhoeae': true,
+    'N. Meningitis': true
+  },
+  'Anaerobes': true,
+  'Atypicals': true
+};
