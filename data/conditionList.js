33fa44bce6394f39a79d61eb71d9c0020582c90d
Gavin Ovsak <ovsak.gavin@gmail.com>
2022-07-10 22:55:58 -0400

    Update conditionList.js

--- data/conditionList.js
+++ data/conditionList.js
@@ -1,5 +1,5 @@
 var conditionList = [
-  { name: 'Skin Incision Ppx', includes: {'MRSA': 0.1, 'MSSA': 1, 'Streptococcus': 1, 'Staphylococcus Epidermidis': 1, 'E. Coli': 1}},
+  { name: 'Skin Incision Ppx', includes: {'MRSA': 0.01, 'MSSA': 1, 'Streptococcus': 1, 'Staphylococcus Epidermidis': 1, 'E. Coli': 1}},
   { name: 'Cellulitis', includes: {'MRSA': 0.4, 'MSSA': 0.4, 'Streptococcus': 0.2 }},
   { name: 'GI Surgery Ppx', includes: {'MRSA': 0.1, 'MSSA': 1, 'Streptococcus': 1, 'E. Coli': 1, 'Salmonella': 1, 'N. Gonorrhoeae': 1, 'Anaerobes': 1}},
   { name: 'UTI - Uncomplicated', includes: {'E. Coli': 0.9} }
