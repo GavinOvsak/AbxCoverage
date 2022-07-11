5b1fd2942acc021f06bd958380e4dee1c907c579
Gavin Ovsak <ovsak.gavin@gmail.com>
2022-07-10 22:29:11 -0400

    u

--- data/conditionList.js
+++ data/conditionList.js
@@ -4,3 +4,5 @@ var conditionList = [
   { name: 'GI Surgery Ppx', includes: {'MRSA': 0.1, 'MSSA': 1, 'Streptococcus': 1, 'E. Coli': 1, 'Salmonella': 1, 'N. Gonorrhoeae': 1, 'Anaerobes': 1}},
   { name: 'UTI - Uncomplicated', includes: {'E. Coli': 0.9} }
 ];
+
+// Dapto, Nafcillin, Amox, Cefotetan, Ceftriaxone, Flagyl
